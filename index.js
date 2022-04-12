b4bdc77a64b14a88565f5a94d63913b58c1d885a
alade93 <99254792+alade93@users.noreply.github.com>
2022-04-12 01:54:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// const countries = [
+//   "Albania",
+//   "Bolivia",
+//   "Canada",
+//   "Denmark",
+//   "Ethiopia",
+//   "Finland",
+//   "Germany",
+//   "Hungary",
+//   "Ireland",
+//   "Japan",
+//   "Kenya",
+// ];
+// console.log(countries);
+// console.log(Array.isArray("Ethiopia"));
+
+// ////
+// const webTechs = [
+//   "HTML",
+//   "CSS",
+//   "JavaScript",
+//   "React",
+//   "Redux",
+//   "Node",
+//   "MongoDB",
+// ];
+// console.log(webTechs);
+// console.log(Array.isArray("Sass"));
+// webTechs.push("Sass");
+// console.log(webTechs);
